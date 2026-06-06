5c8a954fe8afb734561a9431707a6ffaf7b4d2d5
alicvo <141070918+alicvo@users.noreply.github.com>
2026-06-05 19:11:55 -0700

    fixed dropper test script

--- modules/dropper/test_dropper.py
+++ modules/dropper/test_dropper.py
@@ -1,6 +1,8 @@
 import time
 
 from modules.dropper.dropper_wrapper import DropperWrapper
+from modules.motors import MotorWrapper
+from shared_memory import SharedMemoryWrapper
 
 '''
     discord: @alicvo
@@ -12,10 +14,13 @@ from modules.dropper.dropper_wrapper import DropperWrapper
 port="/dev/ttyUSB0"
 BAUDRATE=115200
 
+shared_memory_object = SharedMemoryWrapper()
+M = MotorWrapper(shared_memory_object)
+dropper = DropperWrapper(shared_memory_object)
+
 if __name__ == "__main__":
-    dropper = DropperWrapper(port=port, baudrate=BAUDRATE)
     dropper.drop()
+    M.send_command()  # send drop command
     time.sleep(2)
     dropper.reset()
-    dropper.close()
-    
+    M.send_command()  # send reset command
